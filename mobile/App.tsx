8f0078747983d5621cfdd6b8e495e32ace7b4f6a
Fekleite <fernandacarolineleite360@gmail.com>
2020-10-17 13:53:30 -0300

    feat: create routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { PROVIDER_GOOGLE,  Marker, Callout } from "react-native-maps";
-import { Feather } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
 
-import mapMarker from "./src/assets/map-marker.png";
+import Routes from "./src/routes"
 
-export default function App() {
+const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
     Nunito_600SemiBold,
     Nunito_700Bold,
@@ -19,98 +16,8 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} 
-        style={styles.map} 
-        initialRegion={{
-          latitude: -19.9379478,
-          longitude: -43.1795432,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker 
-          icon={mapMarker}
-          coordinate={{
-            latitude: -19.9379478,
-            longitude: -43.1795432,
-          }}
-          calloutAnchor={{
-            x: 3,
-            y: 0.8
-          }}
-        >
-          <Callout tooltip={true} onPress={() => {}}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das Meninas</Text>
-            </View>
-          </Callout>
-        </Marker>
-      </MapView>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
-
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={()=>{}}>
-          <Feather name="plus" size={20} color="#fff" />
-        </TouchableOpacity>
-      </View>
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-
-  calloutContainer: {
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    borderRadius: 16,
-    justifyContent: "center",
-  },
-
-  calloutText: {
-    fontFamily: "Nunito_700Bold",
-    color: "#0089a5",
-    fontSize: 14
-  },
-
-  footer: {
-    position: "absolute",
-    left: 24,
-    right: 24,
-    bottom: 32,
-    backgroundColor: "#fff",
-    borderRadius: 20,
-    height: 56,
-    paddingLeft: 24,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    elevation: 3,
-  },
-
-  footerText: {
-    fontFamily: "Nunito_700Bold",
-    color: "#8fa7b3"
-  },
-
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    backgroundColor: "#15c3d6",
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
+export default App;
