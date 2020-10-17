9079cfec3d9062ed1e0ca0f085ee971aeee9d05e
Fekleite <fernandacarolineleite360@gmail.com>
2020-10-17 14:49:26 -0300

    feat: add all pages

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { PROVIDER_GOOGLE,  Marker, Callout } from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import mapMarker from "../assets/map-marker.png";
+
 
 
 const OrphanagesMap: React.FC = () => {
@@ -12,6 +14,10 @@ const OrphanagesMap: React.FC = () => {
 
   function handleNavigateToOrphanageDetails() {
     navigation.navigate("OrphanageDetails");
+  }
+
+  function handleNavigateToCreateOrphanage(){
+    navigation.navigate("SelectMapPosition")
   }
 
   return (
@@ -48,9 +54,9 @@ const OrphanagesMap: React.FC = () => {
       <View style={styles.footer}>
         <Text style={styles.footerText}>2 orfanatos encontrados</Text>
 
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={()=>{}}>
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
           <Feather name="plus" size={20} color="#fff" />
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   );
